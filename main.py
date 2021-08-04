75c2712fbeb7ddae24be3bd98dcd83ff3215b0f7
PhellipeDuarte <phellipe.duarte@icloud.com>
2021-08-04 15:49:18 -0300

    Adição da classe Enemy e sua movimentação/spawn

--- main.py
+++ main.py
@@ -39,23 +39,63 @@ class Ship:
         self.lasers = []
         self.cool_down_counter = 0
     
+    # Método para desenhar a nave na tela
     def draw(self, window):
-        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
+        window.blit(self.ship_img, (self.x, self.y))
+    
+    # Métodos para receber o tamanho da imagem da nave
+    def get_width(self):
+        return self.ship_img.get_width()
+    
+    def get_height(self):
+        return self.ship_img.get_height()
+
+# Classe do player, herda da classe abstrata "Nave"
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = YELLOW_SPACE_SHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+
+# Classe dos inimigos, herda da classe abstrata "Nave"
+class Enemy(Ship):
+    COLOR_MAP = {
+        "red":(RED_SPACE_SHIP, RED_LASER),
+        "green":(GREEN_SPACE_SHIP, GREEN_LASER),
+        "blue":(BLUE_SPACE_SHIP, BLUE_LASER),
+    }
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+
+    # Método que realiza a movimentação do inimigo
+    def move(self, vel):
+        self.y += vel
 
 # Definindo o loop principal de execução
 def main():
     run = True
     FPS = 300
-    level = 1
+    level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+    player_vel = 4
+    enemy_vel = 2
 
-    ship = Ship(425, 700)
+    enemies = []
+    wave_lenght = 5
+
+    player = Player(425, 700)
 
     clock = pygame.time.Clock()
 
+    # Função que atualiza a tela no tempo de clock (300 vezez por segundo)
     def redraw_window():
         WIN.blit(BG, (0, 0))
+
         # Escrevendo o texto na tela
         lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
         level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
@@ -63,15 +103,54 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
-        ship.draw(WIN)
+        # Desenhando os inimigos na tela
+        for enemy in enemies:
+            enemy.draw(WIN)
+        
+        player.draw(WIN)
 
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
-        redraw_window()
+
+        if len(enemies) == 0:
+            level += 1
+            wave_lenght += 2
+            for i in range(wave_lenght):
+                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
+                enemies.append(enemy)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                      
+        keys = pygame.key.get_pressed()
+
+        # Indo para a esquerda
+        if keys[pygame.K_a] and player.x + player_vel > 0:
+            player.x -= player_vel
+
+        # Indo para a direita
+        if keys[pygame.K_d] and player.x + player_vel < WIDTH - player.get_width():
+            player.x += player_vel
+
+        # Indo para cima
+        if keys[pygame.K_w] and player.y + player_vel > 0:
+            player.y -= player_vel
+
+        # Indo para baixo
+        if keys[pygame.K_s] and player.y + player_vel < HEIGHT - player.get_height():
+            player.y += player_vel
+
+        # Usa o método mover
+        for enemy in enemies:
+            enemy.move(enemy_vel)
+            
+            # Condicional que percebe se algum inimigo passou pela nave aliada
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+        
+        redraw_window()
+
 main()
